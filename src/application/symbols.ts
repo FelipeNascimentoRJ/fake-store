ca5f2fc0d4863c32f338a021194c23ed3c76471e
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-18 20:06:00 -0300

    feat: add symbols

---
+++ src/application/symbols.ts
@@ -0,0 +1,10 @@
+const Symbols = {
+  repositories: {
+    product: Symbol.for('product.repository'),
+  },
+  adapters: {
+    clientHttp: Symbol.for('adapters.clientHttp'),
+  },
+};
+
+export default Symbols;
