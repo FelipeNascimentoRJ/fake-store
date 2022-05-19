02d93f65fdaefaad994a38c9636c31bef218054c
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-18 22:16:46 -0300

    feat: add ClientStorageAsyncStorageAdapter in setup

--- src/application/setup.ts
+++ src/application/setup.ts
@@ -3,9 +3,14 @@ import Container from './container';
 import {ProductRepositoryImplementation} from './core/repository/product';
 import {CategoryRepositoryImplementation} from './core/repository/category';
 import ClientHttpAxiosAdapter from './infra/http/client-http-axios-adapter';
+import ClientStorageAsyncStorageAdapter from './infra/storage/client-storage-async-storage-adapter';
 
 // Register adapters
 Container.register(Symbols.adapters.clientHttp, ClientHttpAxiosAdapter);
+Container.register(
+  Symbols.adapters.clientStorage,
+  ClientStorageAsyncStorageAdapter,
+);
 
 // Register repositories
 Container.register(
