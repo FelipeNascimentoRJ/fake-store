3c72c806408cb2f6ba8d76834547ab33f1db45cb
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-18 22:52:12 -0300

    refactor: add cart repository contract

--- src/application/domain/repository/cart.ts
+++ src/application/domain/repository/cart.ts
@@ -2,10 +2,10 @@ import {CartEntity} from '../entity/cart';
 import {ProductEntity} from '../entity/product';
 
 export interface CartRepository {
-  setCart: (cart: CartEntity) => void;
+  setCart: (cart: CartEntity) => Promise<void>;
   getCart: () => Promise<CartEntity>;
-  insertProduct: (product: ProductEntity) => void;
-  updateProduct: (productId: number, quantity: number) => void;
-  deleteProduct: (productId: number) => void;
-  clear(): void;
+  insertProduct: (product: ProductEntity) => Promise<void>;
+  updateProduct: (productId: number, quantity: number) => Promise<void>;
+  deleteProduct: (productId: number) => Promise<void>;
+  clear(): Promise<void>;
 }
