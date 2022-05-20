2d66b0c5985a2589016e81b909fd2e789a82f331
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-20 15:39:53 -0300

    feat: add ConfirmationScreen structure

--- src/presentation/screens/confirmation/styles.ts
+++ src/presentation/screens/confirmation/styles.ts
@@ -2,13 +2,11 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
-  background-color: ${({theme}) => theme.colors.background};
+  background-color: ${({theme}) => theme.colors.primary};
 `;
 
-export const Title = styled.Text`
-  font-size: 20px;
-  font-weight: bold;
-  color: ${({theme}) => theme.colors.primary};
+export const Content = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 `;
