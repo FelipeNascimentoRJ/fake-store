7854852b5b797c2ce38a52fb5a18e7df917e5186
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-20 13:35:40 -0300

    feat: add CartItem component

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -3,6 +3,7 @@ export {default as Icon} from './icons';
 export {default as Header} from './header';
 export {default as Button} from './button';
 export {default as Text} from './typography';
+export {default as CartItem} from './cart-item';
 export {default as ProductCard} from './product-card';
 export {default as FooterActionButton} from './footer-action-button';
 
