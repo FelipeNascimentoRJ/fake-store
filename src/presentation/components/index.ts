8ce84fce432956c328e7b769beabba6ee8b5c50c
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-20 00:24:27 -0300

    feat: add exports component

---
+++ src/presentation/components/index.ts
@@ -0,0 +1,9 @@
+export {default as Chip} from './chip';
+export {default as Icon} from './icons';
+export {default as Header} from './header';
+export {default as Button} from './button';
+export {default as Text} from './typography';
+export {default as ProductCard} from './product-card';
+export {default as FooterActionButton} from './footer-action-button';
+
+export * from './product-card';
