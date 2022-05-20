d4ae35452043f640dd98060cbfce252623fb2d0f
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-20 00:23:47 -0300

    feat: add Chip component

---
+++ src/presentation/components/chip/styles.ts
@@ -0,0 +1,29 @@
+import styled, {css} from 'styled-components/native';
+
+export type ChipProps = {
+  checked: boolean;
+};
+
+export const Container = styled.TouchableOpacity<ChipProps>`
+  height: 21px;
+  padding: 6px 10px;
+  margin-right: 12px;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 8px;
+  background-color: ${({theme}) => theme.colors.grayLight};
+  ${({checked, theme}) =>
+    checked &&
+    css`
+      background-color: ${theme.colors.primary};
+      border: 1px solid ${theme.colors.primary};
+    `}
+`;
+
+export const Text = styled.Text<ChipProps>`
+  font-weight: 700;
+  font-size: 8px;
+  line-height: 9px;
+  color: ${({theme, checked}) =>
+    checked ? theme.colors.white : theme.colors.text};
+`;
