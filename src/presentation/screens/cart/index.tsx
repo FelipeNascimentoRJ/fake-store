49b3fc7d51a3ccf041d5dcc470a5eaf72598d60d
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-20 13:36:44 -0300

    feat: add CartScreen structure

--- src/presentation/screens/cart/index.tsx
+++ src/presentation/screens/cart/index.tsx
@@ -1,11 +1,104 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 
-import {Container, Title} from './styles';
+import {
+  Header,
+  Text,
+  Icon,
+  CartItem,
+  FooterActionButton,
+} from '../../components';
+
+import {useTheme} from '../../theme';
+
+import {
+  Container,
+  HeaderBackButton,
+  Content,
+  ListCartContainer,
+  TotalPayableContainer,
+} from './styles';
+
+import cart from './helper';
 
 const CartScreen: React.FC = () => {
+  const theme = useTheme();
+
+  const totalPayable = Object.values(cart).reduce((total, data) => {
+    return total + data.product.price * data.quantity;
+  }, 0);
+
+  const navigateToBack = useCallback(() => {
+    console.log('Navigate to back');
+  }, []);
+
+  const navigateToConfirmation = useCallback(() => {
+    console.log('Navigate to confirmation');
+  }, []);
+
+  const onAddOnePress = useCallback((data: any) => {
+    console.log('Add one product to cart', data);
+  }, []);
+
+  const onReduceOnePress = useCallback((data: any) => {
+    console.log('Reduce one product to cart', data);
+  }, []);
+
+  const renderBackButton = (
+    <HeaderBackButton onPress={navigateToBack}>
+      <Icon name="IconArrowLeft" />
+    </HeaderBackButton>
+  );
+
+  const renderHeader = (
+    <Header
+      left={renderBackButton}
+      center={<Text variant="HeaderTitle">CARRINHO</Text>}
+    />
+  );
+
+  const renderListCart = (
+    <ListCartContainer>
+      {Object.values(cart).map(item => (
+        <CartItem
+          key={item.product.id}
+          data={item}
+          onAddOnePress={onAddOnePress}
+          onReduceOnePress={onReduceOnePress}
+        />
+      ))}
+    </ListCartContainer>
+  );
+
+  const renderTotalPayable = (
+    <TotalPayableContainer>
+      <Text variant="TotalPayable">Total:</Text>
+      <Text variant="TotalPayable">{`$${totalPayable}`}</Text>
+    </TotalPayableContainer>
+  );
+
+  const renderFooter = (
+    <FooterActionButton
+      title="Finalizar compra"
+      rounded={false}
+      withShadow={false}
+      onPress={navigateToConfirmation}
+      color={theme.colors.background}
+    />
+  );
+
+  const renderContent = (
+    <Content>
+      <Text variant="SectionTitle">Meu Carrinho</Text>
+      {renderListCart}
+    </Content>
+  );
+
   return (
     <Container>
-      <Title>CartScreen</Title>
+      {renderHeader}
+      {renderContent}
+      {renderTotalPayable}
+      {renderFooter}
     </Container>
   );
 };
--- src/presentation/screens/cart/styles.ts
+++ src/presentation/screens/cart/styles.ts
@@ -2,13 +2,32 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${({theme}) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-  font-size: 20px;
-  font-weight: bold;
-  color: ${({theme}) => theme.colors.primary};
+export const HeaderBackButton = styled.TouchableOpacity`
+  width: 10px;
+  height: 10px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+})``;
+
+export const ListCartContainer = styled.View`
+  margin-top: 45px;
+  padding: 0 16px;
+`;
+
+export const TotalPayableContainer = styled.View`
+  height: 40px;
+  padding: 0 16px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-top-width: 1px;
+  border-bottom-width: 1px;
+  border-color: ${({theme}) => theme.colors.grayLight};
 `;
