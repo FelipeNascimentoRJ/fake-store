b96616660431e64398bb7b09b781f9a69b62ea74
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-21 16:47:45 -0300

    refactor: remove enum

--- src/presentation/screens/index.ts
+++ src/presentation/screens/index.ts
@@ -1,9 +1,3 @@
 export {default as HomeScreen} from './home';
 export {default as CartScreen} from './cart';
 export {default as ConfirmationScreen} from './confirmation';
-
-export enum Screens {
-  Home = 'HomeScreen',
-  Cart = 'CartScreen',
-  Confirmation = 'ConfirmationScreen',
-}
