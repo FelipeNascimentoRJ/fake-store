59db4357acfe93f3991a51d4da10d5eb1b780255
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-21 21:35:21 -0300

    feat: add use case load categories

---
+++ src/application/core/use-case/load-categories.ts
@@ -0,0 +1,75 @@
+import Symbols from '../../symbols';
+import Container from '../../container';
+import {CategoryRepository} from '../../domain/repository/category';
+import {ClientStorageAdapter} from '../protocols/adapter/client-storage';
+import {LoadCategoriesUseCase} from '../../domain/use-case/load-categories';
+
+export class LoadCategoriesUseCaseImplementation
+  implements LoadCategoriesUseCase
+{
+  private readonly repository: CategoryRepository;
+  private readonly clientStorage: ClientStorageAdapter | undefined;
+  private static readonly KEY_STORAGE = '@FakeStore/categories';
+
+  constructor() {
+    if (!Container.has(Symbols.repositories.category)) {
+      throw new Error('CategoryRepository not registered');
+    }
+
+    this.repository = Container.resolve<CategoryRepository>(
+      Symbols.repositories.category,
+    ) as CategoryRepository;
+
+    if (Container.has(Symbols.adapters.clientStorage)) {
+      this.clientStorage = Container.resolve<ClientStorageAdapter>(
+        Symbols.adapters.clientStorage,
+      ) as ClientStorageAdapter;
+    }
+  }
+
+  public async execute(): Promise<string[]> {
+    try {
+      let categories = await this.loadCache();
+
+      if (categories.length === 0) {
+        categories = await this.repository.getAllCategories();
+
+        if (categories.length >= 1) {
+          // won't wait for return
+          this.saveCache(categories);
+        }
+      }
+
+      return categories;
+    } catch (error) {
+      return [];
+    }
+  }
+
+  private async loadCache(): Promise<string[]> {
+    if (!this.clientStorage) {
+      return [];
+    }
+
+    const categories = await this.clientStorage.select<string[]>(
+      LoadCategoriesUseCaseImplementation.KEY_STORAGE,
+    );
+
+    if (categories && Array.isArray(categories)) {
+      return categories;
+    }
+
+    return [];
+  }
+
+  private async saveCache(categories: string[]): Promise<void> {
+    if (!this.clientStorage || categories.length === 0) {
+      return;
+    }
+
+    await this.clientStorage.insert(
+      LoadCategoriesUseCaseImplementation.KEY_STORAGE,
+      categories,
+    );
+  }
+}
