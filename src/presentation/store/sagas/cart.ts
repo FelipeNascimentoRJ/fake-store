984a541cba8f24ab818c6dba8f948db8aa6f2f77
Felipe Nascimento <felipe.rj@hotmail.com.br>
2022-05-21 21:36:08 -0300

    feat: add connection use case cart in sagas

--- src/presentation/store/sagas/cart.ts
+++ src/presentation/store/sagas/cart.ts
@@ -1,15 +1,48 @@
 import {PayloadAction} from '@reduxjs/toolkit';
 import {all, put, takeLatest} from 'redux-saga/effects';
 
+import Symbols from '../../../application/symbols';
+import Container from '../../../application/container';
+
+import {LoadCartUseCase} from '../../../application/domain/use-case/load-cart';
+import {ClearCartUseCase} from '../../../application/domain/use-case/clear-cart';
+import {AddCartProductUseCase} from '../../../application/domain/use-case/add-cart-product';
+import {ReduceCartProductUseCase} from '../../../application/domain/use-case/reduce-cart-product';
+
 import {Cart} from '../reducers/cart';
 import {ErrorState, ProductState} from '../types';
+
+function* loadMiddleware(): unknown {
+  try {
+    const useCase = Container.resolve<LoadCartUseCase>(
+      Symbols.useCases.loadCart,
+    );
+
+    if (!useCase) {
+      return;
+    }
+
+    const cart = yield useCase.execute();
+
+    yield put(Cart.actions.load(cart));
+  } catch (error) {
+    return;
+  }
+}
 
 function* addOneMiddleware({
   payload: product,
 }: PayloadAction<ProductState>): unknown {
   try {
-    // TODO: add products to cart
-    console.log('addOneMiddleware');
+    const useCase = Container.resolve<AddCartProductUseCase>(
+      Symbols.useCases.addCartProduct,
+    );
+
+    if (!useCase) {
+      return;
+    }
+
+    yield useCase.execute(product);
 
     yield put(Cart.actions.addOne(product));
   } catch (error) {
@@ -21,8 +54,15 @@ function* reduceOneMiddleware({
   payload: productId,
 }: PayloadAction<number>): unknown {
   try {
-    // TODO: reduce products in cart
-    console.log('reduceOneMiddleware');
+    const useCase = Container.resolve<ReduceCartProductUseCase>(
+      Symbols.useCases.reduceCartProduct,
+    );
+
+    if (!useCase) {
+      return;
+    }
+
+    yield useCase.execute(productId);
 
     yield put(Cart.actions.reduceOne(productId));
   } catch (error) {
@@ -32,8 +72,15 @@ function* reduceOneMiddleware({
 
 function* clearMiddleware(): unknown {
   try {
-    // TODO: clear cart
-    console.log('clearMiddleware');
+    const useCase = Container.resolve<ClearCartUseCase>(
+      Symbols.useCases.clearCart,
+    );
+
+    if (!useCase) {
+      return;
+    }
+
+    yield useCase.execute();
 
     yield put(Cart.actions.clear());
   } catch (error) {
@@ -43,8 +90,9 @@ function* clearMiddleware(): unknown {
 
 export default function* root() {
   yield all([
+    takeLatest(Cart.actions.loadMiddleware, loadMiddleware),
+    takeLatest(Cart.actions.clearMiddleware, clearMiddleware),
     takeLatest(Cart.actions.addOneMiddleware, addOneMiddleware),
     takeLatest(Cart.actions.reduceOneMiddleware, reduceOneMiddleware),
-    takeLatest(Cart.actions.clearMiddleware, clearMiddleware),
   ]);
 }
